00cfd6704b3969d3c33ec6958cc2d8137273200e
oppytut <11549752+oppytut@users.noreply.github.com>
2025-09-26 09:57:02 +0700

    add employee test

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   testDir: 'tests/e2e',
   use: {
     browserName: 'chromium',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost',
   },
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
