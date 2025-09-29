51d31e32f90d28b7940389cb219ca18b8f414a46
Arief Novianto <arief.novianto@varx.id>
2025-09-29 18:52:24 +0700

    update test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,21 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // Seed default test user
         User::firstOrCreate(
             ['email' => 'test@example.com'],
             [
                 'name' => 'Test User',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ]
+        );
+
+        // Seed admin user for Playwright test
+        User::firstOrCreate(
+            ['email' => 'admin@admin.com'],
+            [
+                'name' => 'Admin User',
                 'password' => Hash::make('password'),
                 'email_verified_at' => now(),
             ]
