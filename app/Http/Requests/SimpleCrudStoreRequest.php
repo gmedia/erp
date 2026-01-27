24acec222a540e7eda4266054a3b345b0f06c77b
oppytut <11549752+oppytut@users.noreply.github.com>
2026-01-27 12:31:11 +0700

    refactor: make SimpleCrudStoreRequest abstract and remove the concrete getModelClass method.

--- app/Http/Requests/SimpleCrudStoreRequest.php
+++ app/Http/Requests/SimpleCrudStoreRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 
-class SimpleCrudStoreRequest extends FormRequest
+abstract class SimpleCrudStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -23,10 +23,5 @@ class SimpleCrudStoreRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255', 'unique:' . $this->getModelClass() . ',name'],
         ];
-    }
-
-    public function getModelClass(): string
-    {
-        return User::class;
     }
 }
