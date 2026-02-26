b9d4442d20694e3ac86f8890d74e899e960e7e14
oppytut <11549752+oppytut@users.noreply.github.com>
2026-02-26 16:16:02 +0000

    refactor: remove custom toArray method and Request import from PipelineAuditTrailCollection.

--- app/Http/Resources/PipelineAuditTrail/PipelineAuditTrailCollection.php
+++ app/Http/Resources/PipelineAuditTrail/PipelineAuditTrailCollection.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources\PipelineAuditTrail;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PipelineAuditTrailCollection extends ResourceCollection
@@ -13,25 +12,4 @@ class PipelineAuditTrailCollection extends ResourceCollection
      * @var class-string
      */
     public $collects = PipelineAuditTrailResource::class;
-
-    public function toArray(Request $request): array
-    {
-        return [
-            'data' => $this->collection,
-            'meta' => [
-                'current_page' => $this->resource->currentPage(),
-                'last_page' => $this->resource->lastPage(),
-                'per_page' => $this->resource->perPage(),
-                'total' => $this->resource->total(),
-                'from' => $this->resource->firstItem(),
-                'to' => $this->resource->lastItem(),
-            ],
-            'links' => [
-                'first' => $this->resource->url(1),
-                'last' => $this->resource->url($this->resource->lastPage()),
-                'prev' => $this->resource->previousPageUrl(),
-                'next' => $this->resource->nextPageUrl(),
-            ],
-        ];
-    }
 }
