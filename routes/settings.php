9f06a4d4509be6a76867496709cec9ce8bce8477
oppytut <11549752+oppytut@users.noreply.github.com>
2026-03-09 02:51:00 +0000

    refactor: Delete numerous ERP module API route files.

--- routes/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\Settings\PasswordController;
-use App\Http\Controllers\Settings\ProfileController;
-use App\Http\Controllers\Settings\TwoFactorAuthenticationController;
-use Illuminate\Support\Facades\Route;
