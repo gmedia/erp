3ec70d26925e53148e4f9122fd910b5c98e40947
oppytut <11549752+oppytut@users.noreply.github.com>
2026-06-29 08:47:00 +0000

    fix(ci): grant sail user access to MYSQL_DATABASE in mariadb init script

--- docker/mariadb/create-testing-database.sh
+++ docker/mariadb/create-testing-database.sh
@@ -8,5 +8,6 @@ if [ -n "$MYSQL_USER" ]; then
 /usr/bin/mariadb --user=root --password="$MYSQL_ROOT_PASSWORD" <<-EOSQL
     CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
     GRANT ALL PRIVILEGES ON \`testing%\`.* TO '$MYSQL_USER'@'%';
+    GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}%\`.* TO '$MYSQL_USER'@'%';
 EOSQL
 fi
